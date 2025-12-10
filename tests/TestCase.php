e5045abf30ef695a7d1555e6992a2fc69ecaf94f
Milad <miladghorbani006@gmail.com>
2025-12-10 21:30:22 +0330

    test: add base test case class

---
+++ tests/TestCase.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Milad\FilamentArCaptchaField\Tests;
+
+use Milad\FilamentArCaptchaField\FilamentArCaptchaFieldServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+class TestCase extends Orchestra
+{
+    protected function getPackageProviders($app): array
+    {
+        return [
+            FilamentArCaptchaFieldServiceProvider::class,
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app): void
+    {
+        $app['config']->set('filament-arcaptcha-field.site_key', 'test-site-key');
+        $app['config']->set('filament-arcaptcha-field.secret_key', 'test-secret-key');
+        $app['config']->set('filament-arcaptcha-field.options', [
+            'lang' => 'fa',
+            'theme' => 'light',
+        ]);
+    }
+}
+
