5ab318bb70a8ed8480c454d35f8e72a2ac03270f
Milad <miladghorbani006@gmail.com>
2025-12-11 11:02:34 +0330

    fix: update paths for configuration and views in FilamentArCaptchaFieldServiceProvider

--- src/FilamentArCaptchaFieldServiceProvider.php
+++ src/FilamentArCaptchaFieldServiceProvider.php
@@ -9,16 +9,16 @@ class FilamentArCaptchaFieldServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->publishes([
-            __DIR__ . '/../../config/filament-arcaptcha-field.php' => config_path('filament-arcaptcha-field.php'),
+            __DIR__ . '/../config/filament-arcaptcha-field.php' => config_path('filament-arcaptcha-field.php'),
         ], 'config');
 
-        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'filament-arcaptcha-field');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'filament-arcaptcha-field');
     }
 
     public function register(): void
     {
         $this->mergeConfigFrom(
-            __DIR__ . '/../../config/filament-arcaptcha-field.php',
+            __DIR__ . '/../config/filament-arcaptcha-field.php',
             'filament-arcaptcha-field'
         );
     }
